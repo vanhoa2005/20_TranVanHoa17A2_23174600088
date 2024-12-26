7baf39d6c174e962c129ff4509f1353a7d83134e
Trần Hòa <148509921+vanhoa2005@users.noreply.github.com>
2024-12-26 08:39:37 +0700

    Add files via upload

---
+++ 21_Hòa_28022005/Lab3/lab3.2.py
@@ -0,0 +1,21 @@
+# XỬ LÝ DỮ LIỆU NULL
+import pandas as pd
+# 1. Kiểm tra xem trong stocks1 có dữ liệu Null nào không
+stocks1 = pd.read_csv('21_Hòa_28022005\Lab3\stocks1.csv')
+print("\nKiểm tra dữ liệu Null:")
+print(stocks1.isnull().sum())
+
+# 2. Thay thế dữ liệu Null ở cột high bằng giá trị trung bình của cột high
+if 'high' in stocks1.columns:
+    mean_high = stocks1['high'].mean()
+    stocks1['high'].fillna(mean_high, inplace=True)
+
+# 3. Thay thế dữ liệu Null ở cột low bằng giá trị trung bình của cột low
+if 'low' in stocks1.columns:
+    mean_low = stocks1['low'].mean()
+    stocks1['low'].fillna(mean_low, inplace=True)
+
+# 4. Hiển thị thông tin tổng quan để xác nhận không còn dữ liệu Null
+print("\nThông tin tổng quan sau khi xử lý dữ liệu Null:")
+print(stocks1.info())
+
